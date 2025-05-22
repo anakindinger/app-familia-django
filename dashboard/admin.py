ff02b32655baacf63afa456611c37f927bed5422
Ana Beatriz Kindinger <07839955983@tjpr.jus.br>
2025-05-22 18:35:34 -0300

    feature: criado area admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Child, School, Health
 
+admin.site.register(Child)
+admin.site.register(School)
+admin.site.register(Health)
 # Register your models here.
--- dashboard/models.py
+++ dashboard/models.py
@@ -3,11 +3,13 @@ from django.db import models
 # Create your models here.
 class Child(models.Model):
     name = models.CharField(max_length=100)
-    dn = models.DateField()
+    dn = models.DateField("data de nascimento")
     height = models.IntegerField()
     weight = models.IntegerField()
     #school = models.ForeignKey('School', on_delete=models.CASCADE)
     #health = models.ForeignKey('Health', on_delete=models.CASCADE)
+    def __str__(self):
+        return super().__str__()
 
 class School(models.Model):
     child = models.ForeignKey(Child, on_delete=models.CASCADE)
@@ -15,9 +17,13 @@ class School(models.Model):
     telephone = models.CharField(max_length=15)
     classroom = models.CharField(max_length=10)
     teacher = models.CharField(max_length=100)
+    def __str__(self):
+        return super().__str__()
 
 class Health(models.Model):
     child = models.ForeignKey(Child, on_delete=models.CASCADE)
     alergy = models.CharField(max_length=100)
     plan = models.CharField(max_length=100)
     medication = models.CharField(max_length=100)
+    def __str__(self):
+        return super().__str__()
