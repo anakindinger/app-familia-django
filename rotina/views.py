3032b1dcc4834ad8f8c86c7aebd97e4ef8915722
Ana Beatriz Kindinger <07839955983@tjpr.jus.br>
2025-05-28 19:04:08 -0300

    rotinas amanha implementado

--- rotina/views.py
+++ rotina/views.py
@@ -12,12 +12,15 @@ from datetime import date
 def rotina_hoje(request):
     today = date.today()
     # Django: weekday() -> segunda=0, domingo=6. Seu Weekday usa: domingo=1, segunda=2, ...
-    weekday_map = {0: '2', 1: '3', 2: '4', 3: '5', 4: '6', 5: '7', 6: '1'}
+    weekday_map = {0: '1', 1: '2', 2: '3', 3: '4', 4: '5', 5: '6', 6: '7'}
     weekday_str = weekday_map[today.weekday()]
+    weekday_tmr = weekday_map[today.weekday()+1]
     # Filtra rotinas que contenham o dia de hoje (ManyToMany)
-    routine_today = Routine.objects.all()
+    routine_today = Routine.objects.filter(days_of_week__id=weekday_str).order_by('start_time')
+    routine_tomorrow = Routine.objects.filter(days_of_week__id=weekday_tmr).order_by('start_time')
     context = {
         'rotina_hoje': routine_today,
         'data_atual': today,
+        'rotina_amanha':routine_tomorrow,
     }
     return render(request, 'rotina/list_routines.html', context)
