45b2289f35cc6784130bb6ab24f0f8933a743fa8
Ana Beatriz Kindinger <07839955983@tjpr.jus.br>
2025-06-03 16:11:23 -0300

    calendario com alteração de eventos ok

--- agenda/urls.py
+++ agenda/urls.py
@@ -5,5 +5,8 @@ app_name = 'agenda'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cadastrar/', views.cadastrar_evento, name='cadastrar_evento'),
+    path('excluir/<int:evento_id>/', views.excluir_evento, name='excluir_evento'),
+    path('alterar/<int:evento_id>/', views.alterar_evento, name='alterar_evento'),
     
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Evento
 import calendar
 from datetime import date
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from usuario.models import UsuarioChild
 from dashboard.models import Child
 
@@ -56,4 +57,59 @@ def index(request):
         'weekday_str': weekday_str,
         'weekday_tmr': weekday_tmr,
     }
+    return render(request, 'agenda/calendario.html', context)
+
+@csrf_exempt
+@login_required
+def cadastrar_evento(request):
+    children_ids = UsuarioChild.objects.filter(user=request.user).values_list('child_id', flat=True)
+    if request.method == 'POST':
+        descricao = request.POST.get('description')
+        hour_init = request.POST.get('hour_init')
+        hour_end = request.POST.get('hour_end')
+        date = request.POST.getlist('date')
+        child_id = request.POST.get('child_id')
+        if not child_id or int(child_id) not in children_ids:
+            return redirect('agenda:index')
+        evento = Evento.objects.create(
+            description=descricao,
+            date=date,
+            hour_init=hour_init,
+            hour_end=hour_end,
+            child_id=child_id
+        )
+        
+        evento.save()
+        return redirect(f"{request.path}?child_id={child_id}")
+    else:
+        children = Child.objects.filter(id__in=children_ids)
+        selected_child = children.first() if children else None
+        context = {'children': children, 'selected_child': selected_child}
+        return render(request, 'agenda/calendario.html', context)
+
+
+@csrf_exempt
+@login_required
+def excluir_evento(request, evento_id):
+    if request.method == 'POST':
+        Evento.objects.filter(id=evento_id).delete()
+    return redirect('agenda:index')
+
+@csrf_exempt
+@login_required
+def alterar_evento(request, evento_id):
+    evento = get_object_or_404(Evento, id=evento_id)
+    children_ids = UsuarioChild.objects.filter(user=request.user).values_list('child_id', flat=True)
+    if evento.child_id not in children_ids:
+        return redirect('evento:index')
+    if request.method == 'POST':
+        evento.descricao = request.POST.get('description')
+        evento.hour_init = request.POST.get('hour_init')
+        evento.hour_end = request.POST.get('hour_end')
+        evento.date = request.POST.getlist('date')
+        evento.save()
+        return redirect(f"{request.path}?child_id={evento.child_id}")
+  
+    children = Child.objects.filter(id__in=children_ids)
+    context = {'evento': evento, 'children': children, 'selected_child': evento.child}
     return render(request, 'agenda/calendario.html', context)
